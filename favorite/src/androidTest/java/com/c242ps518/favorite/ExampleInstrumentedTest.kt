d66a18b3e7c66017457af6cafbf75b6e10ecd496
rizkimaulana32 <sijond34@gmail.com>
2025-04-03 22:32:35 +0700

    change to base module

--- favorite/src/androidTest/java/com/c242ps518/favorite/ExampleInstrumentedTest.kt
+++ favorite/src/androidTest/java/com/c242ps518/favorite/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.c242ps518.favorite", appContext.packageName)
+        assertEquals("com.c242ps518.topanimeairing", appContext.packageName)
     }
 }
