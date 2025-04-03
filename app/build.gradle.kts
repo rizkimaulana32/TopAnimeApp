9ef8b1000d20ef71f6dbc62360dbbfb33c9c13d8
rizkimaulana32 <sijond34@gmail.com>
2025-04-03 23:57:13 +0700

    add leak canary

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    debugImplementation(libs.leakcanary.android)
 
     implementation(libs.koin.androidx.compose)
 }
